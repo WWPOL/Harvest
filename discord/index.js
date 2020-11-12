60bbd90430d4ef39a345e575dd129bd7cf366e6f
Rudhra Raveendran <rudytheninja@gmail.com>
2020-11-11 21:25:06 -0500

    finished fetch command frontend

--- discord/index.js
+++ discord/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const search = require(`./plugins/${process.env.SEARCH_PLUGIN}`);
 
-client.on('ready', async () => {
+client.on("ready", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
@@ -12,7 +12,18 @@ const HELP_STRING = `Harvest accepts the following commands:
 - /h fetch <file_name>: returns a list of matching files and prompts user to select one
 \`\`\``;
 
-const REACTIONS = ["\u0030\u20E3","\u0031\u20E3","\u0032\u20E3","\u0033\u20E3","\u0034\u20E3","\u0035\u20E3", "\u0036\u20E3","\u0037\u20E3","\u0038\u20E3","\u0039\u20E3"];
+const REACTIONS = [
+  "\u0030\u20E3",
+  "\u0031\u20E3",
+  "\u0032\u20E3",
+  "\u0033\u20E3",
+  "\u0034\u20E3",
+  "\u0035\u20E3",
+  "\u0036\u20E3",
+  "\u0037\u20E3",
+  "\u0038\u20E3",
+  "\u0039\u20E3",
+];
 
 const asyncForEach = async (array, callback) => {
   for (let index = 0; index < array.length; index++) {
@@ -21,21 +32,35 @@ const asyncForEach = async (array, callback) => {
   }
 };
 
-client.on('message', async msg => {
+const listMessages = {};
+client.on("message", async (msg) => {
   // if channel id is set, only process if in proper channel, otherwise process regardless
-  if ((process.env.CHANNEL_ID && msg.channel.id === process.env.CHANNEL_ID) || !process.env.CHANNEL_ID) {
+  if (
+    (process.env.CHANNEL_ID && msg.channel.id === process.env.CHANNEL_ID) ||
+    !process.env.CHANNEL_ID
+  ) {
+    const authorId = msg.author.id;
     const args = msg.content.split(" ");
     // all harvest commands are prefaced with /h
     if (args[0] !== "/h") return;
     switch (args[1]) {
       case "fetch":
-        const results = await search(args.slice(2).join(" "));
+        const allResults = await search(args.slice(2).join(" "));
+        const results = allResults.slice(0, 10); // just in case
         let reply = "Please select an option:\n```";
-        results.forEach((item, i) => reply += `\n - ${i+1}: ${item.name}`);
+        results.forEach(
+          (item, i) =>
+            (reply += `\n - ${i}: ${item.name} [size: ${item.size}, seeders: ${item.seeders}]`)
+        );
         reply += "```";
-        msg.reply(reply).then(async msg => {
+        msg.reply(reply).then(async (listMsg) => {
+          listMessages[authorId] = {
+            msgId: msg.id,
+            listMsgId: listMsg.id,
+            results,
+          };
           await asyncForEach(results, async (_, i) => {
-            await msg.react(REACTIONS[i + 1]);
+            await listMsg.react(REACTIONS[i]);
           });
         });
         break;
@@ -43,8 +68,24 @@ client.on('message', async msg => {
         msg.reply(HELP_STRING);
         break;
       default:
-        msg.reply(`"${args[1]}" is not a valid command! Please use \`/h help\` to see the list of valid commands.`)
+        msg.reply(
+          `"${args[1]}" is not a valid command! Please use \`/h help\` to see the list of valid commands.`
+        );
         break;
+    }
+  }
+});
+
+client.on("messageReactionAdd", async (reaction, user) => {
+  if (listMessages.hasOwnProperty(user.id)) {
+    const { msgId, listMsgId, results } = listMessages[user.id];
+    if (reaction.message.id === listMsgId) {
+      delete listMessages[user.id];
+      const i = REACTIONS.indexOf(reaction.emoji.name);
+      const msg = await reaction.message.channel.messages.fetch(msgId);
+      msg.reply(
+        `Going to download:\n\`\`\`${JSON.stringify(results[i], null, 2)}\`\`\``
+      );
     }
   }
 });
--- discord/plugins/example.js
+++ discord/plugins/example.js
@@ -1,22 +1,33 @@
+const crypto = require("crypto");
+
 // import a search module or write one yourself
-const searchModule = (query) => new Promise((resolve) => {
-     setTimeout(() => {
-       resolve([
-        {
-          name: query + "_1",
-          link: "https://stackoverflow.com/questions/30047205/how-can-i-check-if-an-environment-variable-is-set-in-node-js"
-        },
-        {
-          name: query.toUpperCase() + "_2",
-          link: "https://stackoverflow.com/questions/58858782/using-the-dynamic-import-function-on-node-js"
-        },
-        {
-          name: query + "_3",
-          link: "https://stackoverflow.com/questions/31354559/using-node-js-require-vs-es6-import-export/31367852#31367852"
-        }
-      ]);
-     }, 3000);
-   });
+const searchModule = (query) =>
+  new Promise((resolve) => {
+    const separators = [" ", ".", "-", "_"];
+    const randomSep = () =>
+      separators[Math.floor(Math.random() * separators.length)];
+    const genName = () =>
+      query
+        .split("")
+        .map((c) => {
+          if (separators.includes(c)) {
+            return randomSep();
+          } else if (Math.random() > 0.5) {
+            return c.toUpperCase();
+          } else {
+            return c.toLowerCase();
+          }
+        })
+        .join("");
+
+    const magStart = "magnet:?xt=urn:btih:";
+    const genLink = () => magStart + crypto.randomBytes(32).toString("hex");
+
+    const genItem = () => ({ name: genName(), link: genLink() });
+    setTimeout(() => {
+      resolve(Array.from({ length: 15 }, genItem));
+    }, 3000);
+  });
 
 const search = async (query) => {
   // change query to new format if needed
@@ -25,8 +36,8 @@ const search = async (query) => {
   // get results from your search module
   const results = await searchModule(transformed);
 
-  // make sure results is an object of form [{name: string, url: string}]
-  return results.map(({name, link}) => ({name, url: link}));
-}
+  // make sure results is an object of form [{name: string, size: string, seeders: int, url: string}], and no more than 10 items
+  return results.slice(0, 3).map(({ name, link }) => ({ name, url: link }));
+};
 
 module.exports = search;
